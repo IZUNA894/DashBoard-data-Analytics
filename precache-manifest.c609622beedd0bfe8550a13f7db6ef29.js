7503fbd88ff5d8d67d4f7c92e59ae705a4572e9a
IZUNA894 <jainsanyam1261@gmail.com>
2020-09-20 22:07:52 +0530

    Updates

---
+++ precache-manifest.c609622beedd0bfe8550a13f7db6ef29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc8a816e7b086a42dde6f89c935978b",
+    "url": "/DashBoard-data-Analytics/index.html"
+  },
+  {
+    "revision": "8966bc4f3717b95d1440",
+    "url": "/DashBoard-data-Analytics/static/css/2.eca172c9.chunk.css"
+  },
+  {
+    "revision": "0c5efe1533bac64494a6",
+    "url": "/DashBoard-data-Analytics/static/css/main.27378f0f.chunk.css"
+  },
+  {
+    "revision": "8966bc4f3717b95d1440",
+    "url": "/DashBoard-data-Analytics/static/js/2.b11e06d0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/DashBoard-data-Analytics/static/js/2.b11e06d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5efe1533bac64494a6",
+    "url": "/DashBoard-data-Analytics/static/js/main.032d2807.chunk.js"
+  },
+  {
+    "revision": "ba5fdea79c1e2afa170a",
+    "url": "/DashBoard-data-Analytics/static/js/runtime-main.1a451194.js"
+  }
+]);
